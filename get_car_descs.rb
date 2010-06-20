e0e68e4174f0a6e09470061bd4951121a866ea39
Justin Bailey <justinb@cs.pdx.edu>
2010-06-20 02:57:24 -0700

    Added descriptoin of successful cars.

--- get_car_descs.rb
+++ get_car_descs.rb
@@ -1,24 +1,27 @@
 require 'rubygems'
 require 'mechanize'
+require 'pathname'
 
 cnt = 0  
-lines = open(ARGV[0], "r").gets(nil).collect { |l| l.split("\t")[0].strip }
+# Argument must be path to succeeded.txt
+# Puts descriptions in that directory, named succeeded_descs.txt
+lines = open(Pathname.new(ARGV[0]), "r").gets(nil).collect { |l| l.split("\t")[0].strip }
 car_ids = lines.slice(2, lines.length) 
 
 agent = Mechanize.new
-login = agent.get('http://icfpcontest.org/icfp10/login')
-login_form = login.form('f')
-
-login_form.j_username = "ooplss2010"
-login_form.j_password = "1484968454509167853383152085303289771444721966896581041165450"
-
-agent.submit login_form
-
-open("get_cars.txt", "w") do |f|
+open(Pathname.new(ARGV[0]).dirname + "succeeded_descs.txt", "w") do |f|
   f.puts <<-EOS
 CAR ID\tDESCRIPTION
 ======\t===========
 EOS
+  login = agent.get('http://icfpcontest.org/icfp10/login')
+  login_form = login.form('f')
+  
+  login_form.j_username = "ooplss2010"
+  login_form.j_password = "1484968454509167853383152085303289771444721966896581041165450"
+  
+  agent.submit login_form
+  
   car_list = agent.get('http://icfpcontest.org/icfp10/score/instanceTeamCount')
   car_list.forms.each do |car_form|
     car_id = car_form.action.slice(/icfp10\/instance\/(\d+)\//, 1)
