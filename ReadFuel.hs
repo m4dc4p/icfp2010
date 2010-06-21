f898fa71196cdbe315945d9595de802b14868f52
Justin Bailey <justinb@cs.pdx.edu>
2010-06-20 20:16:00 -0700

    Removed dependency on strict package.

--- ReadFuel.hs
+++ ReadFuel.hs
@@ -1,6 +1,5 @@
 module Main where
 import Text.ParserCombinators.Parsec
-import qualified System.IO.Strict as S
 import System.Environment (getArgs)
 
 -- | A tank is a list of ingredient coeffecients
@@ -13,9 +12,17 @@ type Ingredients = [Ingredient]
 -- | An ingredient coeffecient is an integer
 type Ingredient = Integer
 
-fuel input = do
-  let parseFuel = (try blankLines <|> empty) >> many1 tank
-  return $ parse parseFuel "unknown" input
+main = do
+  args <- getArgs
+  let file = case args of
+               (f : _) -> f
+               _ -> error $ "Must provide input file name in args: " ++ show args
+  f <- parseFromFile fuel file
+  case f of 
+    Left err -> putStrLn (show err)
+    Right tanks -> mapM_ (putStrLn . show ) tanks
+
+fuel = (try blankLines <|> empty) >> many1 tank
   
 tank = do
   ingrs <- many1 coeffs
@@ -61,12 +68,3 @@ blanks = skipMany (satisfy (`elem` [' ', '\t']))
 empty :: GenParser Char st ()
 empty = return ()
 
-main = do
-  args <- getArgs
-  desc <- case args of
-            (i : _) -> S.readFile i
-            _ -> error $ "Must provide input file name in args: " ++ show args
-  f <- fuel desc
-  case f of 
-    Left err -> putStrLn (show err)
-    Right tanks -> mapM_ (putStrLn . show ) tanks
